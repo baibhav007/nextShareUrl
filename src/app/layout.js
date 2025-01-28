53d60ca1b7b881866e0bc90fdb5565b3935946ff
baibhav.kumar <baibhav.kumar@loyaltty.com>
2025-01-28 11:19:18 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,15 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="/loyaltty.png" />
+        {/* Static Open Graph Metadata */}
+        <meta property="og:title" content="Welcome to Next Share URL BY BAIBHAV" />
+        <meta property="og:description" content="BAIBHAV Explore amazing deals and offers!" />
+        <meta property="og:image" content="https://next-share-url.vercel.app/loyaltty.png" />
+        <meta property="og:url" content="https://next-share-url.vercel.app" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Welcome to Next Share URL" />
+        <meta name="twitter:description" content="Explore amazing deals and offers!" />
+        <meta name="twitter:image" content="https://next-share-url.vercel.app/loyaltty.png" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
