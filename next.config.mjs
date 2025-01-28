e6db99aace699e219f0761b82246eab980135376
baibhav.kumar <baibhav.kumar@loyaltty.com>
2025-01-28 11:34:56 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['storage.googleapis.com'],
+  },
 
-    images: {
-        domains: ['storage.googleapis.com'],
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply to all pages
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store', // Disable caching for all pages
+          },
+        ],
       },
+    ]
+  },
 };
 
 export default nextConfig;
