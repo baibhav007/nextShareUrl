f87e95fd9d01b4166e1e3542a414c8e45df2502c
baibhav.kumar <baibhav.kumar@loyaltty.com>
2025-01-28 12:46:00 +0530

    changes in the code

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    // Redirect after 5 seconds
+    const timer = setTimeout(() => {
+      window.location.replace("https://loyaltty.com");
+    }, 5000); // 5000ms = 5 seconds
+
+    // Cleanup timer when component is unmounted
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <h1>Hello Baibhav111</h1>
+    <div>
+      <h1>Hello Loyaltty</h1>
+      <p>Redirecting you to the new domain...</p>
+    </div>
   );
 }
